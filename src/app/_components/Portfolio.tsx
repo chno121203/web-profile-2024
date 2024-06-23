8629e41a1a4bb72d97d6be698d613a0d912d6a61
chno121203 <151304005+chno121203@users.noreply.github.com>
2024-06-23 13:28:35 +0700

    bg

--- src/app/_components/Portfolio.tsx
+++ src/app/_components/Portfolio.tsx
@@ -13,7 +13,7 @@ const Portfolio = (props: Props) => {
               href="https://github.com/golang/go"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-blue-500"
+              className="text-red-900 bg-white"
             >
               Go - Programming Language
             </a>
@@ -23,7 +23,7 @@ const Portfolio = (props: Props) => {
               href="https://github.com/wu-web-camp/web-profile-2024"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-blue-500"
+              className="text-red-900 bg-white"
             >
               web-profile-2024 - Personal Website
             </a>
