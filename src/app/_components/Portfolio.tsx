f1f93d8e46c84631539e744b6a11aded056cb3db
chno121203 <151304005+chno121203@users.noreply.github.com>
2024-06-23 13:59:53 +0700

    Update Portfolio.tsx

--- src/app/_components/Portfolio.tsx
+++ src/app/_components/Portfolio.tsx
@@ -23,7 +23,7 @@ const Portfolio = (props: Props) => {
               href="https://github.com/wu-web-camp/web-profile-2024"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-red-900 bg-white"
+              className="text-white bg-red-900"
             >
               web-profile-2024 - Personal Website
             </a>
